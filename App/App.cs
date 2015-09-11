3b81ad0b104aeaf0faa311ecad6ad89450d1e8c1
h3tch <michael.hecher@gmx.net>
2015-09-11 14:47:40 +0200

    added ScintillaNET references extend env. variable path at runtime for ScintillaNET updated OpenTK to support short 'unsigned' types like 'ushort'

--- App/App.cs
+++ App/App.cs
@@ -22,6 +22,15 @@ namespace gled
         {
             InitializeComponent();
 
+            // ADD LIB FOLDER TO ENVIRONMENT VARIABLE PATH
+            // this is necessary for ScintillaNET
+            var envPath = Environment.GetEnvironmentVariable("PATH");
+            var dir = Directory.GetCurrentDirectory();
+            var lib = dir + "\\..\\..\\lib";
+            if (!envPath.Contains(lib))
+                Environment.SetEnvironmentVariable("PATH", envPath + ";" + lib);
+
+            // select 'float' as the default buffer value type
             this.comboBufType.SelectedIndex = 7;
 
             AddSourceTab(@"../../demos/fragoutput.tech");
