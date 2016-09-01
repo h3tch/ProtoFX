9fa258b81482247e0dc330353b3cf0941e625043
h3tch <michael.hecher@gmx.net>
2016-09-01 10:00:01 +0200

    Refactoring

--- App/Properties/Settings.Designer.cs
+++ App/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace gled.Properties {
+namespace App.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
